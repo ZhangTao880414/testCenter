9ea5ddac5211550ef3dca8f54b4f6e0c1a908e9c
zhangtao <zhangtao@n-sight.com.cn>
2023-08-15 22:26:49 +0800

    add all

--- ToraOps/myapps/toraapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ToraappConfig(AppConfig):
-    name = 'toraapp'
--- deploy/ToraOps/myapps/tora_monitor/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ToraMonitorConfig(AppConfig):
-    name = 'tora_monitor'
